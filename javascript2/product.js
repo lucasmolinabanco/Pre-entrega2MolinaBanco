28008573e06f6195d3c2ea410fa1f9c19a460546
lucasmolinabanco <131321228+lucasmolinabanco@users.noreply.github.com>
2023-08-24 14:58:34 -0300

    Add files via upload

---
+++ javascript2/product.js
@@ -0,0 +1,35 @@
+const productos = [
+    {
+        id:1,
+        nombre: "remera",
+        precio: 500,
+       img:
+            "https://deliver.com.ar/wp-content/uploads/2021/09/REMERA-OVERSIZE-22-PORTADA-36-scaled.jpg",
+    },
+    {
+        id:2,
+        nombre: "pantalon",
+        precio: 1000,
+        img:
+        "https://media.revistagq.com/photos/5ca5eca84c7adb138100c90a/1:1/w_320,h_320,c_limit/el_pantalon_de_vestir_894893361.jpg",
+    },
+    {
+        id:3,
+        nombre: "campera",
+        precio: 1500,
+        img:"https://drifters.com.ar/uploads/product_image/28488/650w_DriftersPDP_DQ6279-010_Shot1.jpg",
+    },
+    {
+        id:4,
+        nombre: "zapatilla",
+        precio: 2000,
+        img:"https://calzadosnicoar.vtexassets.com/arquivos/ids/68242229/Zapatilla-urbana-negro-mujer-kevingston-lucy-a_dyjooo.jpg?v=638278236695870000",
+    },
+    {
+        id:5,
+        nombre: "sudadera",
+        precio: 1400,
+        img:"https://s3.sa-east-1.amazonaws.com/www.vaypol.com.ar/variants/t41l14ed9qtgor9qtndxe9ub0qt3/c77c2a06864ac9aca38dc5bd9371de015471edcdbf322dfb14411689bf968ae5",
+    },
+
+]
